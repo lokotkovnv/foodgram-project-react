623fa25106b5d0d92f14e2ff220666fe1546732e
lokotkovnv <burn153@gmail.com>
2024-04-10 18:39:14 +0300

    Fix Remarks

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('api/', include('foodgram.urls')),
     path('api/users/', include('users.urls')),
     path('api/auth/', include('djoser.urls.authtoken')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
