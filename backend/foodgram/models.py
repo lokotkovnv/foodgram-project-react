916c63629f2d7fab5645f155f2a714cfea058647
lokotkovnv <burn153@gmail.com>
2024-04-10 20:36:13 +0300

    Fix Models

--- backend/foodgram/models.py
+++ backend/foodgram/models.py
@@ -1,7 +1,7 @@
-from django.contrib.auth import get_user_model
-from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
 
 User = get_user_model()
 
